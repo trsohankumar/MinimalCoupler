6bb8eaf31a69b15a282424dbde79d9afdd971c61
trsohankumar <sohankumartr@gmail.com>
2025-12-27 11:58:12 +0100

    refactor: Update Participant class to use string_view for participant names and mesh dimensions

--- participant/Participant.cpp
+++ participant/Participant.cpp
@@ -1,111 +1,104 @@
 #include "Participant.hpp"
-#include "FluidParticipantImpl.hpp"
-#include "SolidParticipantImpl.hpp"
+#include "fluid/FluidParticipantImpl.hpp"
+#include "solid/SolidParticipantImpl.hpp"
 
-#include<stdexcept>
+#include <stdexcept>
 namespace precice
 {
 
-Participant::Participant(
-    std::string participantName,
-    std::string configurationFileName,
-    int solverProcessIndex,
-    int solverProcessSize)
-{
-    // create appropriate participant based on participantName
-    if (participantName == "Fluid")
+    Participant::Participant(::precice::string_view participantName, ::precice::string_view configurationFileName, int solverProcessIndex, int solverProcessSize)
     {
-        _impl = std::make_unique<MinimalCoupler::FluidParticipantImplementation>(
-            std::move(participantName),
-            std::move(configurationFileName),
-            solverProcessIndex,
-            solverProcessSize
-        );
-    } 
-    else if (participantName == "Solid") 
-    {
+        if (participantName == "Fluid")
+        {
+            _impl = std::make_unique<MinimalCoupler::FluidParticipantImplementation>(
+                std::move(participantName),
+                std::move(configurationFileName),
+                solverProcessIndex,
+                solverProcessSize);
+        }
+        else if (participantName == "Solid")
+        {
 
-        _impl = std::make_unique<MinimalCoupler::SolidParticipantImplementation>(
-            std::move(participantName),
-            std::move(configurationFileName),
-            solverProcessIndex,
-            solverProcessSize
-        );
+            _impl = std::make_unique<MinimalCoupler::SolidParticipantImplementation>(
+                std::move(participantName),
+                std::move(configurationFileName),
+                solverProcessIndex,
+                solverProcessSize);
+        }
+        else
+        {
+            throw std::invalid_argument("Unknown participant name:" + std::string(participantName));
+        }
     }
-    else 
+
+    int Participant::getMeshDimensions(::precice::string_view meshName) const
     {
-        throw std::invalid_argument("Unknown participant name:" + participantName);
+        return _impl->getMeshDimensions(meshName);
     }
-}
 
-int Participant::getMeshDimensions(const std::string& meshName) const
-{
-    return _impl->getMeshDimensions(meshName);
-}
+    void Participant::setMeshVertices(::precice::string_view meshName,
+                                      ::precice::span<const double> coordinates, ::precice::span<VertexID> ids)
+    {
+        _impl->setMeshVertices(meshName, coordinates, ids);
+    }
 
-void Participant::setMeshVertices(::precice::string_view meshName,
-::precice::span< const double > coordinates, ::precice::span< VertexID > ids)
-{
-    _impl->setMeshVertices(meshName, positions, ids);
-}
+    void Participant::readData(const std::string &meshName, const std::string &dataName, const std::vector<int> &vertexIDs, double relativeReadTime, std::vector<double> &values) const
+    {
+        _impl->readData(meshName, dataName, vertexIDs, relativeReadTime, values);
+    }
 
-void Participant::readData( const std::string& meshName, const std::string& dataName, const std::vector<int>& vertexIDs, double relativeReadTime, std::vector<double>& values) const
-{
-    _impl->readData(meshName, dataName, vertexIDs, relativeReadTime, values);
-}
+    void Participant::writeData(const std::string &meshName, const std::string &dataName, const std::vector<int> &vertexIDs, const std::vector<double> &values)
+    {
+        _impl->writeData(meshName, dataName, vertexIDs, values);
+    }
 
-void Participant::writeData(const std::string& meshName, const std::string& dataName, const std::vector<int>& vertexIDs, const std::vector<double>& values)
-{
-    _impl->writeData(meshName, dataName, vertexIDs, values);
-}
+    void Participant::initialize()
+    {
+        _impl->initialize();
+    }
 
-void Participant::initialize()
-{
-    _impl->initialize();
-}
+    void Participant::advance(double computedTimeStepSize)
+    {
+        _impl->advance(computedTimeStepSize);
+    }
 
-void Participant::advance(double computedTimeStepSize)
-{
-    _impl->advance(computedTimeStepSize);
-}
+    void Participant::finalize()
+    {
+        _impl->finalize();
+    }
 
-void Participant::finalize()
-{
-    _impl->finalize();
-}
+    bool Participant::isCouplingOngoing() const
+    {
+        return _impl->isCouplingOngoing();
+    }
 
-bool Participant::isCouplingOngoing() const
-{
-    return _impl->isCouplingOngoing();
-}
+    bool Participant::requiresInitialData() const
+    {
+        return _impl->requiresInitialData();
+    }
 
-bool Participant::requiresInitialData() const
-{
-    return _impl->requiresInitialData();
-}
+    bool Participant::requiresWritingCheckpoint() const
+    {
+        return _impl->requiresWritingCheckpoint();
+    }
 
-bool Participant::requiresWritingCheckpoint() const
-{
-    return _impl->requiresWritingCheckpoint();
-}
+    bool Participant::requiresReadingCheckpoint() const
+    {
+        return _impl->requiresReadingCheckpoint();
+    }
 
-bool Participant::requiresReadingCheckpoint() const
-{
-    return _impl->requiresReadingCheckpoint();
-}
+    double Participant::getMaxTimeStepSize() const
+    {
+        return _impl->getMaxTimeStepSize();
+    }
 
-double Participant::getMaxTimeStepSize() const
-{
-    return _impl->getMaxTimeStepSize();
-}
+    void Participant::startProfilingSection(const std::string &name)
+    {
+        _impl->startProfilingSection(name);
+    }
 
-void Participant::startProfilingSection(const std::string& name)
-{
-    _impl->startProfilingSection(name);
-}
-
-void Participant::stopLastProfilingSection()
-{
-    _impl->stopLastProfilingSection();
-}
+    void Participant::stopLastProfilingSection()
+    {
+        _impl->stopLastProfilingSection();
+    }
 } // namespace precice
--- participant/Participant.hpp
+++ participant/Participant.hpp
@@ -14,37 +14,26 @@ namespace MinimalCoupler
 
 namespace precice
 {
-class Participant
-{
-public:
-    Participant(
-      std::string participantName,
-      std::string configurationFileName,
-      int         solverProcessIndex,
-      int         solverProcessSize);
+  class Participant
+  {
+  public:
+    Participant(::precice::string_view participantName, ::precice::string_view configurationFileName, int solverProcessIndex, int solverProcessSize);
 
     ~Participant() = default;
 
     // Mesh methods
-    int getMeshDimensions(const std::string& meshName) const;
+    int getMeshDimensions(::precice::string_view meshName) const;
 
-    void setMeshVertices(
-      ::precice::string_view meshName,
-      ::precice::span< const double > coordinates, ::precice::span< VertexID > ids);
+    void setMeshVertices(::precice::string_view meshName, ::precice::span<const double> coordinates, ::precice::span<VertexID> ids);
 
     // Data exchange methods
-    void readData(
-      const std::string& meshName,
-      const std::string& dataName,
-      const std::vector<int>& vertexIDs,
-      double relativeReadTime,
-      std::vector<double>& values) const;
+    void readData(const std::string &meshName, const std::string &dataName, const std::vector<int> &vertexIDs, double relativeReadTime,std::vector<double> &values) const;
 
     void writeData(
-      const std::string& meshName,
-      const std::string& dataName,
-      const std::vector<int>& vertexIDs,
-      const std::vector<double>& values);
+        const std::string &meshName,
+        const std::string &dataName,
+        const std::vector<int> &vertexIDs,
+        const std::vector<double> &values);
 
     // Steering methods
     void initialize();
@@ -60,11 +49,10 @@ public:
     double getMaxTimeStepSize() const;
 
     // Profiling
-    void startProfilingSection(const std::string& name);
+    void startProfilingSection(const std::string &name);
     void stopLastProfilingSection();
 
-private:
-
+  private:
     std::unique_ptr<MinimalCoupler::ParticipantImplementation> _impl;
-};
+  };
 }
