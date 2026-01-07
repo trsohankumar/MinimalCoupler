f03537d96978a95f81fa263115ed58aee3b68e3c
trsohankumar <sohankumartr@gmail.com>
2026-01-07 08:41:08 +0100

    feat: Add precice.cpp implementation and update precice.hpp with version information function

---
+++ precice/precice.cpp
@@ -0,0 +1,10 @@
+#include "precice.hpp"
+
+namespace precice {
+
+std::string getVersionInformation()
+{
+    return "minimal-coupler v1.0.0 (preCICE API compatible)";
+}
+
+}
--- precice/precice.hpp
+++ precice/precice.hpp
@@ -1,3 +1,10 @@
 #pragma once
 
 #include "../participant/Participant.hpp"
+#include <string>
+
+namespace precice {
+
+std::string getVersionInformation();
+
+}
